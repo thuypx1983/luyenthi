39790a6107e08518a322dc92d12feb83697efcbf
PHAM XUAN THUY <thuypx1983@gmail.com>
2017-06-20 07:22:52 +0700

    New update

--- sites/all/themes/oms/js/scripts.js
+++ sites/all/themes/oms/js/scripts.js
@@ -1,10 +1,58 @@
 (function ($) {
     $(function () {
-        $('.region-header >div.block').matchHeight({
+        $('.region-header >div.block,.contact-us-info>div.col-md-4').matchHeight({
             byRow: true,
             property: 'height',
             target: null,
             remove: false
         });
+
+
+
+
+
+        var type=$('#views-exposed-form-search-tour-page #edit-field-type-value');
+        var destination=$('#views-exposed-form-search-tour-page #edit-term-node-tid-depth');
+        type.change(function(){
+            filterDiemDen(type.val());
+            destination.val('All');
+        })
+        $(function(){
+            filterDiemDen(type.val());
+        })
+        function filterDiemDen(type){
+
+            switch (type){
+                case "0":
+                    destination.find('option').each(function(){
+                        var value=parseInt($(this).attr('value'));
+                        if(diem_den_trong_nuoc.indexOf(value) > -1){
+                            $(this).show();
+                        }else{
+                            if($(this).val()!='All'){
+                                $(this).hide();
+                            }
+
+                        }
+                    })
+
+                    break;
+                case "1":
+                    destination.find('option').each(function(){
+                        var value=parseInt($(this).attr('value'));
+                        if(diem_den_nuoc_ngoai.indexOf(value) > -1){
+                            $(this).show();
+                        }else{
+                            if($(this).val()!='All'){
+                                $(this).hide();
+                            }
+                        }
+                    })
+                    break;
+                default:
+                    destination.find('option').show();
+                    break;
+            }
+        }
     })
 })(jQuery)
---
+++ sites/default/files/languages/vi_VZq5jmwp-Y-L1H613bLdzz-QeTF4uqUU_iM1di389iA.js
@@ -0,0 +1,1 @@
+Drupal.locale = { 'strings': {"":{"Edit":"S\u1eeda","Alias: @alias":"\u0110\u01b0\u1eddng d\u1eabn \u1ea3o: @alias","No alias":"Ch\u01b0a c\u00f3 \u0111\u01b0\u1eddng d\u1eabn \u1ea3o","(active tab)":"(tab ho\u1ea1t \u0111\u1ed9ng)"}} };
