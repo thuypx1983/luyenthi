6440990ba57f98dbce485fd2905d05e9b4218a00
PHAM XUAN THUY <thuypx1983@gmail.com>
2017-07-12 07:34:10 +0700

    Fix filetr luyenthi

--- sites/all/themes/oms/js/scripts.js
+++ sites/all/themes/oms/js/scripts.js
@@ -23,6 +23,15 @@
             var title=$('#quicktabs-tabpage-thi_thu_thcs-2').find('.views-field-title');
             $('#quicktabs-tab-thi_thu_thcs-2').text(title.text());
             title.remove();
+        },
+
+        /**
+         * Disable some category
+         */
+        disablePageCategory: function(){
+            var select=$('#views-exposed-form-quiz-filter-page').find('#edit-shs-term-node-tid-depth-select-1');
+            alert(select.length)
+            select.find('option[value=0],option[value=27]').hide();
         }
 
 
@@ -32,6 +41,7 @@
     $(function () {
         oms.initMatchHeight();
         oms.initQuickTabExams();
+        oms.disablePageCategory();
     })
 
 
