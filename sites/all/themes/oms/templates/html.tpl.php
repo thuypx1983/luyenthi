39311b4233f52dd4885341c769dbc8862b41cdbd
Pham Xuan Thuy <phamxuanthuy@sutunam.com>
2017-08-07 14:31:53 +0700

    meta

--- sites/all/themes/oms/templates/html.tpl.php
+++ sites/all/themes/oms/templates/html.tpl.php
@@ -2,12 +2,15 @@
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 <head>
     <meta name="robots" content="noindex,nofollow" />
-<?php print $head; ?>
-<title><?php print $head_title; ?></title>
-<?php print $styles; ?>
-<?php print $scripts; ?>
-<!--[if lt IE 9]><script src="<?php print base_path() . drupal_get_path('theme', 'oms') . '/js/html5.js'; ?>"></script><![endif]-->
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js"></script>
+    <meta name="MobileOptimized" content="width">
+    <meta name="HandheldFriendly" content="true">
+    <meta name="viewport" content="initial-scale=1.0, width=device-width">
+  <?php print $head; ?>
+    <title><?php print $head_title; ?></title>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+    <!--[if lt IE 9]><script src="<?php print base_path() . drupal_get_path('theme', 'oms') . '/js/html5.js'; ?>"></script><![endif]-->
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js"></script>
 </head>
 <body class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php print $page_top; ?>
