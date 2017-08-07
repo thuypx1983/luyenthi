33dbb97e57e575697259161e0d28b2a50103c930
Pham Xuan Thuy <phamxuanthuy@sutunam.com>
2017-08-07 14:50:32 +0700

    css

--- sites/all/themes/oms/templates/pages/page--front.tpl.php
+++ sites/all/themes/oms/templates/pages/page--front.tpl.php
@@ -150,28 +150,6 @@
       <div class="clear"></div>
   <?php endif; ?>
 
-  <?php if ($page['footer_first'] || $page['footer_second'] || $page['footer_third'] || $page['footer_fourth']): ?>
-      <div id="footer-saran" class=" clearfix">
-          <div id="footer-wrap" class="container">
-              <div class="row">
-                <?php if ($page['footer_first']): ?>
-                    <div class="footer-box col-md-3"><?php print render($page['footer_first']); ?></div>
-                <?php endif; ?>
-                <?php if ($page['footer_second']): ?>
-                    <div class="footer-box col-md-3"><?php print render($page['footer_second']); ?></div>
-                <?php endif; ?>
-                <?php if ($page['footer_third']): ?>
-                    <div class="footer-box col-md-3"><?php print render($page['footer_third']); ?></div>
-                <?php endif; ?>
-                <?php if ($page['footer_fourth']): ?>
-                    <div class="footer-box remove-margin col-md-3"><?php print render($page['footer_fourth']); ?></div>
-                <?php endif; ?>
-              </div>
-          </div>
-      </div>
-      <div class="clear"></div>
-  <?php endif; ?>
-
     <!--END footer -->
   <?php include(drupal_get_path('theme', 'oms').'/templates/pages/footer.tpl.php'); ?>
 </div>
